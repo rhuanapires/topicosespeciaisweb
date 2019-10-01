6ee398a1b082a964fd8618cc3f50aff2c4c75f1a
Glaydson Vasconcelos de Sousa <glaydson.sousa@bcb.gov.br>
2019-10-01 10:55:51 -0300

    Correções após Aula de 30-09

--- agp/src/app/produtos/lista-produtos.component.ts
+++ agp/src/app/produtos/lista-produtos.component.ts
@@ -11,7 +11,6 @@ export class ListaProdutosComponent implements OnInit {
     larguraImagem: number = 50;
     margemImagem: number = 2;
     exibirImagem: boolean = false;
-    filtroLista: string = 'carrinho';
     produtosFiltrados: IProduto[];
     _filtroLista: string;
     get filtroLista() : string {
@@ -21,63 +20,64 @@ export class ListaProdutosComponent implements OnInit {
       this._filtroLista = valor;
       this.produtosFiltrados = this.filtroLista ? this.executarFiltro(this.filtroLista) : this.produtos;
     }
-    produtos: IProduto[] = 
+    produtos: IProduto[] =
     [
-        {
-          "idProduto": 1,
-          "nomeProduto": "Leaf Rake",
-          "codigoProduto": "GDN-0011",
-          "dataLancamento": "March 19, 2019",
-          "descricao": "Leaf rake with 48-inch wooden handle.",
-          "preco": 19.95,
-          "rating": 3.2,
-          "urlImagem": "assets/imagens/leaf_rake.png"
-        },
-        {
-          "idProduto": 2,
-          "nomeProduto": "Garden Cart",
-          "codigoProduto": "GDN-0023",
-          "dataLancamento": "March 18, 2019",
-          "descricao": "15 gallon capacity rolling garden cart",
-          "preco": 32.99,
-          "rating": 4.2,
-          "urlImagem": "assets/imagens/garden_cart.png"
-        },
-        {
-          "idProduto": 5,
-          "nomeProduto": "Hammer",
-          "codigoProduto": "TBX-0048",
-          "dataLancamento": "May 21, 2019",
-          "descricao": "Curved claw steel hammer",
-          "preco": 8.9,
-          "rating": 4.8,
-          "urlImagem": "assets/imagens/hammer.png"
-        },
-        {
-          "idProduto": 8,
-          "nomeProduto": "Saw",
-          "codigoProduto": "TBX-0022",
-          "dataLancamento": "May 15, 2019",
-          "descricao": "15-inch steel blade hand saw",
-          "preco": 11.55,
-          "rating": 3.7,
-          "urlImagem": "assets/imagens/saw.png"
-        },
-        {
-          "idProduto": 10,
-          "nomeProduto": "Video Game Controller",
-          "codigoProduto": "GMG-0042",
-          "dataLancamento": "October 15, 2018",
-          "descricao": "Standard two-button video game controller",
-          "preco": 35.95,
-          "rating": 4.6,
-          "urlImagem": "assets/imagens/xbox-controller.png"
-        }
+      {
+        "idProduto": 1,
+        "nomeProduto": "Ancinho",
+        "codigoProduto": "GDN-0011",
+        "dataLancamento": "19 de Março, 2016",
+        "descricao": "Ancinho com cabo de madeira de 48 polegadas.",
+        "preco": 19.95,
+        "rating": 3.2,
+        "urlImagem": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+      },
+      {
+        "idProduto": 2,
+        "nomeProduto": "Carrinho de Jardim",
+        "codigoProduto": "GDN-0023",
+        "dataLancamento": "18 de março, 2016",
+        "descricao": "Carrinho de Jardim com capacidade de 56 litros",
+        "preco": 32.99,
+        "rating": 4.2,
+        "urlImagem": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+      },
+      {
+        "idProduto": 5,
+        "nomeProduto": "Martelo",
+        "codigoProduto": "TBX-0048",
+        "dataLancamento": "21 de maio, 2016",
+        "descricao": "Martelo de aço de garra curva",
+        "preco": 8.9,
+        "rating": 4.8,
+        "urlImagem": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+      },
+      {
+        "idProduto": 8,
+        "nomeProduto": "Serra",
+        "codigoProduto": "TBX-0022",
+        "dataLancamento": "15 de Maio, 2016",
+        "descricao": "Serra de mão com lâmina de aço de 15 polegadas",
+        "preco": 11.55,
+        "rating": 3.7,
+        "urlImagem": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+      },
+      {
+        "idProduto": 10,
+        "nomeProduto": "Controle de Vídeo Game",
+        "codigoProduto": "GMG-0042",
+        "dataLancamento": "15 de outubro, 2015",
+        "descricao": "Controle de vídeo game padrão com dois botões",
+        "preco": 35.95,
+        "rating": 4.6,
+        "urlImagem": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+      }
     ]
 
     constructor() {
       // Exibe a lista completa
       this.produtosFiltrados = this.produtos;
+      // Configura o valor inicial do filtro
       this.filtroLista = 'carrinho';
     }
 
@@ -92,7 +92,7 @@ export class ListaProdutosComponent implements OnInit {
 
     executarFiltro(filtrarPor: string): IProduto[] {
       filtrarPor = filtrarPor.toLocaleLowerCase();
-      return this.produtos.filter((produto: IProduto) => 
+      return this.produtos.filter((produto: IProduto) =>
         produto.nomeProduto.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
     }
 
